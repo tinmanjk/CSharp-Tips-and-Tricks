1192fa71c03c4cd8268f362ea6c7a84933eca5ca
Nikolay Kostov <admin@nikolay.it>
2015-07-16 12:03:02 +0300

    Added demo for InternalsVisibleTo

---
+++ UnitTesting/ExposeInternals.Tests/SummatorUnitTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ExposeInternals.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SummatorUnitTests
+    {
+        [TestMethod]
+        public void SumTwoPlusTwoShouldEqualFour()
+        {
+            var summator = new Summator();
+            var result = summator.Sum(2, 2);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void SumMinusOneAndMinusOneShouldEqualMinusTwo()
+        {
+            var summator = new Summator();
+            var result = summator.Sum(-1, -1);
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        public void SumInt32MaxValueAndInt32MaxValueShouldProduceCorrectResult()
+        {
+            var summator = new Summator();
+            var result = summator.Sum(int.MaxValue, int.MaxValue);
+            Assert.AreEqual((long)int.MaxValue + int.MaxValue, result);
+        }
+
+        [TestMethod]
+        public void SumInt32MaxValueAndInt32MinValueShouldEqualMinusOne()
+        {
+            var summator = new Summator();
+            var result = summator.Sum(int.MaxValue, int.MinValue);
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
---
+++ UnitTesting/ExposeInternals/Summator.cs
@@ -0,0 +1,11 @@
+﻿namespace ExposeInternals
+{
+    internal class Summator
+    {
+        internal long Sum(int a, int b)
+        {
+            var sum = (long)a + b;
+            return sum;
+        }
+    }
+}
