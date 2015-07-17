0fb5cb7171a7eb69543018a2f0b56ec3f087039c
Nikolay Kostov <admin@nikolay.it>
2015-07-17 11:03:48 +0300

    Improvements in HighQualityRandomNumbers demo

--- SyntacticSugar/Program.cs
+++ SyntacticSugar/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SyntacticSugar
+﻿namespace SyntacticSugar
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
         }
     }
--- Traps/RandomNumbers/HighQualityRandomNumbers.cs
+++ Traps/RandomNumbers/HighQualityRandomNumbers.cs
@@ -7,30 +7,42 @@
     {
         private readonly int numberOfRandomNumbers;
 
-        private readonly RandomNumberGenerator randomNumberGenerator;
+        private readonly RandomNumberGenerator firstRandomNumberGenerator;
+
+        private readonly RandomNumberGenerator secondRandomNumberGenerator;
 
         public HighQualityRandomNumbers(int numberOfRandomNumbers)
         {
             this.numberOfRandomNumbers = numberOfRandomNumbers;
-            this.randomNumberGenerator = RandomNumberGenerator.Create();
+            this.firstRandomNumberGenerator = RandomNumberGenerator.Create();
+            this.secondRandomNumberGenerator = RandomNumberGenerator.Create();
         }
 
         public void PrintRandomNumbers()
         {
-            Console.Write("RNGCryptoServiceProvider: ");
+            Console.Write("First RNGCryptoServiceProvider: ");
             for (int i = 0; i < this.numberOfRandomNumbers; i++)
             {
-                var randomInt32Value = this.GenerateRandomInt32Value();
+                var randomInt32Value = GenerateRandomInt32Value(this.firstRandomNumberGenerator);
+                Console.Write("{0} ", randomInt32Value);
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Second RNGCryptoServiceProvider: ");
+            for (int i = 0; i < this.numberOfRandomNumbers; i++)
+            {
+                var randomInt32Value = GenerateRandomInt32Value(this.secondRandomNumberGenerator);
                 Console.Write("{0} ", randomInt32Value);
             }
 
             Console.WriteLine();
         }
 
-        private int GenerateRandomInt32Value()
+        private static int GenerateRandomInt32Value(RandomNumberGenerator randomNumberGenerator)
         {
             var fourRandomBytes = new byte[4]; // 4 bytes = 32 bits = Int32
-            this.randomNumberGenerator.GetBytes(fourRandomBytes);
+            randomNumberGenerator.GetBytes(fourRandomBytes);
             var randomInt32Value = BitConverter.ToInt32(fourRandomBytes, 0);
             return randomInt32Value;
         }
