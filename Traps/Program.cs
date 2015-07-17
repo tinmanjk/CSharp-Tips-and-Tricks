3e0a814377c84c121600d9cbc729cb4185f35ca9
Nikolay Kostov <admin@nikolay.it>
2015-07-17 11:00:57 +0300

    Added demo for using Random and RNGCryptoServiceProvider

--- Traps/Program.cs
+++ Traps/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Traps
+﻿namespace Traps
 {
-    class Program
+    using Traps.RandomNumbers;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            RandomNumbers();
+        }
+
+        private static void RandomNumbers()
+        {
+            var randomInstancesWithTheSameSeed = new RandomInstancesWithTheSameSeed(4);
+            randomInstancesWithTheSameSeed.PrintRandomNumbers();
+
+            var highQualityRandomNumbers = new HighQualityRandomNumbers(4);
+            highQualityRandomNumbers.PrintRandomNumbers();
         }
     }
 }
---
+++ Traps/RandomNumbers/HighQualityRandomNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace Traps.RandomNumbers
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public class HighQualityRandomNumbers : IRandomNumbersPrinter
+    {
+        private readonly int numberOfRandomNumbers;
+
+        private readonly RandomNumberGenerator randomNumberGenerator;
+
+        public HighQualityRandomNumbers(int numberOfRandomNumbers)
+        {
+            this.numberOfRandomNumbers = numberOfRandomNumbers;
+            this.randomNumberGenerator = RandomNumberGenerator.Create();
+        }
+
+        public void PrintRandomNumbers()
+        {
+            Console.Write("RNGCryptoServiceProvider: ");
+            for (int i = 0; i < this.numberOfRandomNumbers; i++)
+            {
+                var randomInt32Value = this.GenerateRandomInt32Value();
+                Console.Write("{0} ", randomInt32Value);
+            }
+
+            Console.WriteLine();
+        }
+
+        private int GenerateRandomInt32Value()
+        {
+            var fourRandomBytes = new byte[4]; // 4 bytes = 32 bits = Int32
+            this.randomNumberGenerator.GetBytes(fourRandomBytes);
+            var randomInt32Value = BitConverter.ToInt32(fourRandomBytes, 0);
+            return randomInt32Value;
+        }
+    }
+}
