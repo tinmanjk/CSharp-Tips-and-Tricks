22c0d6fda7d526d543bb0bb2b03b524091ec5871
Nikolay Kostov <admin@nikolay.it>
2015-07-20 14:11:31 +0300

    Added demo for the Tuple<> class

--- UsefulClassesAndMethods/Program.cs
+++ UsefulClassesAndMethods/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UsefulClassesAndMethods
+﻿namespace UsefulClassesAndMethods
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+
+    using UsefulClassesAndMethods.Tuple;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            TupleDemo();
+        }
+
+        private static void TupleDemo()
+        {
+            var countAndAverageCalculator = new CountAndAverageCalculator();
+            var result = countAndAverageCalculator.GetCountAndAverage(new List<int> { 1, 2, 3, 4 });
+            Console.WriteLine("Count: {0}", result.Item1);
+            Console.WriteLine("Average: {0}", result.Item2);
+            
+            var anotherResult = countAndAverageCalculator.GetCountAndAverage(new List<int> { 4, 3, 2, 1 });
+            Console.WriteLine(anotherResult.Equals(result)); // anotherResult == result => reference comparison
+
+            // Property or indexer 'System.Tuple<int,decimal>.Item1' cannot be assigned to -- it is read only
+            // result.Item1 = 1;
         }
     }
 }
