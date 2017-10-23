f663f77aad8573e3111dfa19b20ca74ad9d127a5
tinmanjk <tinmanjk@mail.com>
2017-10-23 13:17:02 +0300

    added try catch blocks for exceptions

--- SyntacticSugar/CastingVsAsOperator/Program.cs
+++ SyntacticSugar/CastingVsAsOperator/Program.cs
@@ -15,15 +15,37 @@
                 Console.WriteLine("'number is string' is true");
             }
 
-            //// This will cause an unhandled exception of type 'InvalidCastException' because the specified cast is not valid.
-            //// var numberAsInt = (int?)number;
+            int? numberAsInt = null;
 
-            //// var numberAsInt = number as int?;
-            //// This will cause an unhandled exception of type 'InvalidOperationException' because numberAsInt will be null
-            //// Console.WriteLine(numberAsInt.Value);
 
-            //// When using the 'as' operator we should always consider the possible null value
-            //// Console.WriteLine(numberAsInt.GetValueOrDefault(0));
+            //This will cause an unhandled exception of type 'InvalidCastException' because the specified cast is not valid.
+
+            try
+            {
+                numberAsInt = (int?)number;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType());
+            }
+
+            //This will cause an unhandled exception of type 'InvalidOperationException' because numberAsInt will be null
+
+            try
+            {
+                numberAsInt = number as int?;
+                Console.WriteLine(numberAsInt.Value);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType());
+            }
+
+            //When using the 'as' operator we should always consider the possible null value
+            Console.WriteLine(numberAsInt.GetValueOrDefault(0));
         }
     }
 }
